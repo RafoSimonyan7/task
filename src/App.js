a27204146c4463cb0329f8cba2aec97485a7f15b
RafoSimonyan7 <rafayelsimonyan1998@mail.ru>
2021-02-09 14:25:08 +0400

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,48 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import Cars from "./components/cars";
 
 function App() {
+  const car = [
+    {
+      id: 1,
+      model: "Mercedes",
+      year: 2019,
+      img:
+        "https://avto-russia.ru/autos/mercedes/photo/mercedes_a-class_sedan_small.jpg",
+    },
+    {
+      id: 2,
+      model: "Bmw",
+      year: 2018,
+      img:
+        "https://smartcdn.prod.postmedia.digital/driving/wp-content/uploads/2020/05/chrome-image-410754.png",
+    },
+    {
+      id: 3,
+      model: "Audi",
+      year: 2017,
+      img:
+        "https://quto.ru/service-imgs/5d/8b/6f/f6/5d8b6ff67b89c.jpeg",
+    },
+    {
+      id: 4,
+      model: "Jaguar",
+      year: 2020,
+      img:
+        "https://cars.usnews.com/static/images/Auto/izmo/i157546822/2020_jaguar_xf_angularfront.jpg"
+    },
+  ];
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {car.map(item => (
+        <Cars
+          model={item.model}
+          year={item.year}
+          img={item.img}
+          key={item.id}
+        />
+      ))}
     </div>
   );
 }
---
+++ src/components/cars.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import '../App.css';
+
+const Cars = ({model, year, img}) => {
+    return (
+        <div>
+            <h1>{model}</h1>
+            <h4>{year}</h4>
+            <img src={img} alt="img"/>
+        </div>
+    )
+};
+
+export default Cars;
